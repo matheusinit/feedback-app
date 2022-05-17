5e1270fde1ee897068103972ed630a05a5642c25
matheusinit <matheus.oliveira.s@protonmail.com>
2022-05-17 08:24:54 -0200

    Remove CSS stylesheet

--- index.php
+++ index.php
@@ -10,7 +10,6 @@
   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
   <!-- Bootstrap 5 (CSS) -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-  <link rel="stylesheet" href="css/styles.css">
   <title>Your Feedback</title>
 </head>
 
